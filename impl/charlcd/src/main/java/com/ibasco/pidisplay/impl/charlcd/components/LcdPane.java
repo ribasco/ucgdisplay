81be47964888e5e9446fa1830953d0fee1f38584
Rafael Luis Ibasco <raffy@ibasco.com>
2017-10-26 21:15:42 +0800

    Added lcd implementation

---
+++ impl/charlcd/src/main/java/com/ibasco/pidisplay/impl/charlcd/components/LcdPane.java
@@ -0,0 +1,10 @@
+package com.ibasco.pidisplay.impl.charlcd.components;
+
+import com.ibasco.pidisplay.core.components.DisplayPane;
+import com.ibasco.pidisplay.impl.charlcd.CharGraphics;
+
+public class LcdPane extends DisplayPane<CharGraphics> {
+    public LcdPane() {
+        super();
+    }
+}
---
+++ impl/charlcd/src/main/java/com/ibasco/pidisplay/impl/charlcd/components/LcdTextBox.java
@@ -0,0 +1,43 @@
+package com.ibasco.pidisplay.impl.charlcd.components;
+
+import com.ibasco.pidisplay.core.components.DisplayText;
+import com.ibasco.pidisplay.core.components.DisplayTextBox;
+import com.ibasco.pidisplay.impl.charlcd.CharGraphics;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@SuppressWarnings("WeakerAccess")
+public class LcdTextBox extends DisplayTextBox<CharGraphics> {
+
+    private static final Logger log = LoggerFactory.getLogger(LcdTextBox.class);
+
+    public LcdTextBox() {
+        this(null, null);
+    }
+
+    public LcdTextBox(Integer width, Integer height) {
+        this(null, null, width, height);
+    }
+
+    public LcdTextBox(Integer left, Integer top, Integer width, Integer height) {
+        super(left, top, width, height);
+    }
+
+    @Override
+    protected DisplayText<CharGraphics> createContent() {
+        return new LcdText(0, 0, "Text Box") {
+            @Override
+            protected void drawNode(CharGraphics graphics) {
+                super.drawNode(graphics);
+                if (blinkCursor.get()) {
+                    graphics.cursorBlink(blinkCursor.get());
+                }
+            }
+        };
+    }
+
+    @Override
+    protected void drawNode(CharGraphics graphics) {
+        //log.debug("Drawing LcdTextBox (Text Cursor: x={}, y={})", graphics.getColOffset(), graphics.getRowOffset());
+    }
+}
