da95e8d1dfa7d634cb1b18ddb0113c023f3796fb
Rafael Ibasco <ribasco@gmail.com>
2018-10-21 20:51:06 +0800

    Use tar on unix environment and zip on windows

--- native/src/main/cpp/cmake/FindRpiTools.cmake
+++ native/src/main/cpp/cmake/FindRpiTools.cmake
@@ -13,11 +13,19 @@ endif ()
 
 # Check if the path exists, if it doesn't, download a copy to the source directory
 if (NOT EXISTS ${RPI_TOOLCHAIN_PATH})
-    message(STATUS "[FIND-TOOLCHAIN] Downloading toolchain to ${TOOLS_DIR_PATH}/toolchain.zip")
-    file(DOWNLOAD https://github.com/raspberrypi/tools/archive/master.zip ${TOOLS_DIR_PATH}/toolchain.zip SHOW_PROGRESS)
+    if (UNIX)
+        message(STATUS "[FIND-TOOLCHAIN] Downloading toolchain to ${TOOLS_DIR_PATH}/toolchain.tar.gz")
+        file(DOWNLOAD https://github.com/raspberrypi/tools/archive/master.tar.gz ${TOOLS_DIR_PATH}/toolchain.tar.gz SHOW_PROGRESS)
 
-    message(STATUS "[FIND-TOOLCHAIN] Unzipping '${TOOLS_DIR_PATH}/toolchain.zip' to '${TOOLS_DIR_PATH}'")
-    execute_process(COMMAND unzip ${TOOLS_DIR_PATH}/toolchain.zip WORKING_DIRECTORY ${TOOLS_DIR_PATH} ERROR_VARIABLE tc_unzip)
+        message(STATUS "[FIND-TOOLCHAIN] Unzipping '${TOOLS_DIR_PATH}/toolchain.tar.gz' to '${TOOLS_DIR_PATH}'")
+        execute_process(COMMAND tar xvzf ${TOOLS_DIR_PATH}/toolchain.tar.gz WORKING_DIRECTORY ${TOOLS_DIR_PATH} ERROR_VARIABLE tc_unzip)
+    else()
+        message(STATUS "[FIND-TOOLCHAIN] Downloading toolchain to ${TOOLS_DIR_PATH}/toolchain.zip")
+        file(DOWNLOAD https://github.com/raspberrypi/tools/archive/master.zip ${TOOLS_DIR_PATH}/toolchain.zip SHOW_PROGRESS)
+
+        message(STATUS "[FIND-TOOLCHAIN] Unzipping '${TOOLS_DIR_PATH}/toolchain.zip' to '${TOOLS_DIR_PATH}'")
+        execute_process(COMMAND unzip ${TOOLS_DIR_PATH}/toolchain.zip WORKING_DIRECTORY ${TOOLS_DIR_PATH} ERROR_VARIABLE tc_unzip)
+    endif()
 
     if (tc_unzip)
         message(FATAL_ERROR "[FIND-TOOLCHAIN] Could not unzip contents of the downloaded toolchain (${tc_unzip})")
