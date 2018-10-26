ed2e29934eaf8b160a063f0ebbd6be5b4c8d7406
Rafael Ibasco <ribasco@gmail.com>
2018-10-26 12:30:58 +0800

    Re-organized examples module

--- examples/drivers/clcd/src/main/java/HD44780Example.java
+++ examples/drivers/clcd/src/main/java/HD44780Example.java
@@ -23,7 +23,6 @@
  * <http://www.gnu.org/licenses/lgpl-3.0.html>.
  * =========================END==================================
  */
-package com.ibasco.ucgdisplay.examples;
 
 import com.ibasco.ucgdisplay.drivers.clcd.HD44780DisplayDriver;
 import com.ibasco.ucgdisplay.drivers.clcd.LcdGpioAdapter;
