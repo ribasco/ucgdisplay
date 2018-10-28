380a4eab1f7d3c30f49f3e4d360e63d0170f39f2
Rafael Ibasco <ribasco@gmail.com>
2018-10-28 22:15:05 +0800

    Added description

--- scripts/update-sources.sh
+++ scripts/update-sources.sh
@@ -1,8 +1,18 @@
 #!/bin/bash
 set -e
 
+#
+# ==================================================
+# Steps to update
+# ==================================================
+# 1) Compile codegen
+# 2) Run codegen
+# 3) Commit changes
+#
+
+
 # Switch working directory
 cd ../modules/graphics/src/main/cpp/utils/
 
-cmake --target ucgd-code -G 'CodeBlocks - Unix Makefiles' -H. -Bbuild/linux/arm
+# cmake --target ucgd-code -G 'CodeBlocks - Unix Makefiles' -H. -Bbuild/linux/arm
 
