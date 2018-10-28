180bb8f96fcbc25c161c14e4b54bb571b8c8b358
Rafael Ibasco <ribasco@gmail.com>
2018-10-28 22:14:42 +0800

    Added missing license headers

--- native/modules/graphics/src/main/java/com/ibasco/ucgdisplay/utils/codegen/UpdateControllerHeader.java
+++ native/modules/graphics/src/main/java/com/ibasco/ucgdisplay/utils/codegen/UpdateControllerHeader.java
@@ -1,3 +1,28 @@
+/*-
+ * ========================START=================================
+ * Organization: Universal Character/Graphics display library
+ * Project: UCGDisplay :: Native :: Graphics
+ * Filename: UpdateControllerHeader.java
+ *
+ * ---------------------------------------------------------
+ * %%
+ * Copyright (C) 2018 Universal Character/Graphics display library
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Lesser Public License for more details.
+ *
+ * You should have received a copy of the GNU General Lesser Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/lgpl-3.0.html>.
+ * =========================END==================================
+ */
 package com.ibasco.ucgdisplay.utils.codegen;
 
 import org.slf4j.Logger;
@@ -51,15 +76,17 @@ public class UpdateControllerHeader {
         String codeBuildUrl = getDefault(2, args, DEFAULT_CODEBUILD_URL);
         Path outputFilePath = Paths.get(outputDir + File.separator + outputFile);
 
-        log.info("Output directory: {}", outputDir);
-        log.info("Output file: {}", outputFile);
-        log.info("Output file path: {}", outputFilePath);
-        log.info("Codebuild URL: {}", codeBuildUrl);
+        log.info("[UPDATE-CONTROLLER-H] Output directory: {}", outputDir);
+        log.info("[UPDATE-CONTROLLER-H] Output file: {}", outputFile);
+        log.info("[UPDATE-CONTROLLER-H] Output file path: {}", outputFilePath);
+        log.info("[UPDATE-CONTROLLER-H] Codebuild URL: {}", codeBuildUrl);
 
         try (BufferedInputStream in = new BufferedInputStream(new URL(codeBuildUrl).openStream())) {
+            log.info("[UPDATE-CONTROLLER-H] Successfully downloaded codebuild.c from master (Bytes: " + in.available() + ")");
             StringBuilder controllerCode = new StringBuilder();
             boolean startCollecting = false;
 
+            //Start scanning for the controllers array section
             try (Scanner scanner = new Scanner(in)) {
                 while (scanner.hasNextLine()) {
                     String line = scanner.nextLine();
@@ -79,14 +106,13 @@ public class UpdateControllerHeader {
                         controllerCode.append("\n");
                     }
                 }
-                //log.debug("Controller Code: \n{}", controllerCode.toString());
-                log.info("Exporting controllers.h to '{}'", outputFilePath);
             }
 
             if (controllerCode.length() > 0) {
+                log.info("[UPDATE-CONTROLLER-H] Exporting controllers.h to '{}'", outputFilePath);
 
                 if (Files.deleteIfExists(outputFilePath)) {
-                    log.info("Existing file deleted: {}", outputFilePath);
+                    log.info("[UPDATE-CONTROLLER-H] Removing existing file: {}", outputFilePath);
                 }
                 File file = Files.createFile(outputFilePath).toFile();
                 StringBuilder comment = new StringBuilder();
