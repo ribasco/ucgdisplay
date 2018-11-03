fc16eda998aa805ac22263e163706afdc58fcfc7
Rafael Ibasco <ribasco@gmail.com>
2018-11-03 15:14:51 +0800

    (Issue #17) Uncaught exceptions should be propagated

---
+++ native/modules/graphics/src/main/java/com/ibasco/ucgdisplay/core/u8g2/exceptions/U8g2GpioEventException.java
@@ -0,0 +1,7 @@
+package com.ibasco.ucgdisplay.core.u8g2.exceptions;
+
+public class U8g2GpioEventException extends U8g2EventException {
+    public U8g2GpioEventException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
