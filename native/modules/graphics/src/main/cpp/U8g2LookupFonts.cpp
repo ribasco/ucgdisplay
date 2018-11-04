db9f25b31a5ee0c695002fb44c279a9a1da4694e
Rafael Ibasco <ribasco@gmail.com>
2018-11-05 02:07:41 +0800

    Add Mac OSX support for virtual driver
    Changes:
    
    - Added cross-compilation support for Mac OSX (experimental)
    - Added OSXCROSS_TARGET
    - Updated toolchain archive
    - Renamed osx toolchain file
    - Use the tar command directly as the ant task is discarding the symbolic links
    - Fixed working directory path
    - Updated osx toolchain file
    - Re-structured cross compile configuration entries
    - Added darwin jdk include headers
    - Use CMAKE_SYSTEM_NAME for searching JDK path
    - Use separate ant build file for generating native sources
    - Add mac os search criteria
    - Re-organized target structure
    - Add project.scripts.dir
    - Use tasks instead of target
    - Updated osxcross toolchain. Now compiled with LDFLAGS="-static-libstdc++"
    - Cache osxcross toolchain in travis
    - Updated license headers
    - Improved toolchain check logic
    - Updated project versions
    - Updated project description

--- native/modules/graphics/src/main/cpp/U8g2LookupFonts.cpp
+++ native/modules/graphics/src/main/cpp/U8g2LookupFonts.cpp
@@ -1,5 +1,5 @@
 /*
- * THIS IS AN AUTO-GENERATED CODE!! PLEASE DO NOT MODIFY (Last updated: Nov  4 2018 14:49:09) 
+ * THIS IS AN AUTO-GENERATED CODE!! PLEASE DO NOT MODIFY (Last updated: Nov  5 2018 17:59:35) 
  */
 
 #include "U8g2Hal.h"
