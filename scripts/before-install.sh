355d5018f92a4712886633394e043c157c48b666
Rafael Ibasco <ribasco@gmail.com>
2018-11-09 03:09:44 +0800

    Included extra verbose details regarding the current compiler Disabled sudo

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -11,11 +11,14 @@ echo "######   #######  #        #######  #     #  #######      ###  #     #   #
 
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    #sudo ln -s /usr/bin/gcc-7 /usr/local/bin/gcc
-    #sudo ln -s /usr/bin/g++-7 /usr/local/bin/g++
+    ln -s /usr/bin/gcc-7 /usr/local/bin/gcc
+    ln -s /usr/bin/g++-7 /usr/local/bin/g++
+    which gcc && which g++
     gcc -v && g++ -v && cmake --version
-    #export CC=/usr/bin/gcc-7
-    #export CXX=/usr/bin/g++-7
+    ls -l /usr/bin/g*
+    ls -l /usr/bin/c*
+    export CC=/usr/bin/gcc-7
+    export CXX=/usr/bin/g++-7
     if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then openssl aes-256-cbc -K $encrypted_0b32ef791498_key -iv $encrypted_0b32ef791498_iv -in scripts/travis.enc -out scripts/travis.dec -d; fi
     gpg2 --import scripts/travis.dec
 else
