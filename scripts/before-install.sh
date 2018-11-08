ab55eda2b0ff6ee6c48faa9dd2249e35cb9c78f8
Rafael Ibasco <ribasco@gmail.com>
2018-11-09 01:40:29 +0800

    Updated travis script's file attributes Use sudo: required Fixed to use g++7 Use full-path for CC and CXX envvars

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -11,11 +11,11 @@ echo "######   #######  #        #######  #     #  #######      ###  #     #   #
 
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    sudo ln -s /usr/bin/gcc-7 /usr/local/bin/gcc
-    sudo ln -s /usr/bin/g++-7 /usr/local/bin/g++
+    #sudo ln -s /usr/bin/gcc-7 /usr/local/bin/gcc
+    #sudo ln -s /usr/bin/g++-7 /usr/local/bin/g++
     gcc -v && g++ -v && cmake --version
-    export CC=/usr/bin/gcc-7
-    export CXX=/usr/bin/g++-7
+    #export CC=/usr/bin/gcc-7
+    #export CXX=/usr/bin/g++-7
     if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then openssl aes-256-cbc -K $encrypted_0b32ef791498_key -iv $encrypted_0b32ef791498_iv -in scripts/travis.enc -out scripts/travis.dec -d; fi
     gpg2 --import scripts/travis.dec
 else
