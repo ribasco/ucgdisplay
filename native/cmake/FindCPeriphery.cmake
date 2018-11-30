de4e6f20e4a1d07c51d1fcbde797d53316a49fed
Rafael Ibasco <ribasco@gmail.com>
2018-11-30 17:21:55 +0800

    Add c-periphery package

---
+++ native/cmake/FindCPeriphery.cmake
@@ -0,0 +1,19 @@
+message(STATUS "Including c-periphery")
+include(ExternalProject)
+
+# c-periphery
+ExternalProject_Add(
+        cperiphery
+        GIT_REPOSITORY "https://github.com/vsergeev/c-periphery.git"
+        GIT_TAG "master"
+        UPDATE_COMMAND ""
+        PATCH_COMMAND ""
+        CONFIGURE_COMMAND ""
+        INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
+        SOURCE_DIR "${CMAKE_SOURCE_DIR}/lib/cperiphery"
+        INSTALL_COMMAND make install
+        UPDATE_COMMAND ""
+        BUILD_COMMAND make all
+)
+
+add_dependencies(ucgdisp cperiphery)
