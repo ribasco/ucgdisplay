ccf9401c7ee47d029d89cea6d32de20f0d99973e
Rafael Ibasco <ribasco@gmail.com>
2018-12-01 22:40:13 +0800

    Added build in source property

--- native/cmake/FindCPeriphery.cmake
+++ native/cmake/FindCPeriphery.cmake
@@ -6,14 +6,15 @@ ExternalProject_Add(
         cperiphery
         GIT_REPOSITORY "https://github.com/vsergeev/c-periphery.git"
         GIT_TAG "master"
+        BUILD_IN_SOURCE true
         UPDATE_COMMAND ""
         PATCH_COMMAND ""
         CONFIGURE_COMMAND ""
-        INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
+        #INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
         SOURCE_DIR "${CMAKE_SOURCE_DIR}/lib/c-periphery"
-        INSTALL_COMMAND ""
-        UPDATE_COMMAND ""
-        BUILD_COMMAND "make all"
+        #INSTALL_COMMAND ""
+        #UPDATE_COMMAND ""
+        BUILD_COMMAND make all
 )
 
 add_dependencies(ucgdisp cperiphery)
