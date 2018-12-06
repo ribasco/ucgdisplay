90293c22a4a47d15b6cf3271d8f78b20eed03748
Rafael Ibasco <ribasco@gmail.com>
2018-12-06 17:10:08 +0800

    Added fPIC in CFLAGS

--- native/cmake/FindLibGpiod.cmake
+++ native/cmake/FindLibGpiod.cmake
@@ -13,29 +13,31 @@ if (NOT EXISTS ${PROJ_INCLUDE_DIR}/linux/compiler_types.h)
     file(COPY /usr/src/linux-headers-${KERNEL_VERSION}/include/linux/compiler_types.h DESTINATION ${PROJ_INCLUDE_DIR}/linux)
 endif ()
 
-set(CFLAGS "-I/usr/src/linux-headers-${KERNEL_VERSION}/include/uapi -I${PROJ_INCLUDE_DIR}")
+set(CFLAGS "-I/usr/src/linux-headers-${KERNEL_VERSION}/include/uapi -I${PROJ_INCLUDE_DIR} -fPIC")
 
 message(STATUS "${PROJ_TAG} CFLAGS = ${CFLAGS}, LIB_DIR=${LIB_DIR}")
+
+# ac_cv_func_realloc_0_nonnull=yes
 
 ExternalProject_Add(
         libgpiod
         GIT_REPOSITORY "https://git.kernel.org/pub/scm/libs/libgpiod/libgpiod.git"
-        GIT_TAG "master"
+        GIT_TAG "v1.2"
         STAMP_DIR ${PROJECT_BINARY_DIR}/stamp
         UPDATE_COMMAND ""
-        CONFIGURE_COMMAND ./autogen.sh --enable-tools=no --enable-bindings-cxx --prefix=${CMAKE_INSTALL_PREFIX} --host=arm-linux-gnueabihf CFLAGS=${CFLAGS} CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER}
+        CONFIGURE_COMMAND CC=${CMAKE_C_COMPILER} CXX=${CMAKE_CXX_COMPILER} ./autogen.sh --enable-tools=yes --enable-bindings-cxx --prefix=${CMAKE_INSTALL_PREFIX} --host=arm-linux-gnueabihf CFLAGS=${CFLAGS} ac_cv_func_malloc_0_nonnull=yes
         SOURCE_DIR "${CMAKE_SOURCE_DIR}/lib/libgpiod"
         BINARY_DIR "${CMAKE_SOURCE_DIR}/lib/libgpiod"
         INSTALL_COMMAND make install
         INSTALL_DIR ${CMAKE_INSTALL_PREFIX}
         UPDATE_COMMAND ""
         BUILD_COMMAND make
-        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR> -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER} -DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER} -DCMAKE_SYSROOT=${CMAKE_SYSROOT}
+        CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=<INSTALL_DIR>
 )
 
 set(LIBGPIOD_INCLUDE_DIR ${CMAKE_INSTALL_PREFIX}/include)
-set(LIBGPIOD_LIB_C ${CMAKE_INSTALL_PREFIX}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}gpiod${CMAKE_SHARED_LIBRARY_SUFFIX})
-set(LIBGPIOD_LIB_CXX ${CMAKE_INSTALL_PREFIX}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}gpiodcxx${CMAKE_SHARED_LIBRARY_SUFFIX})
+set(LIBGPIOD_LIB_C ${CMAKE_INSTALL_PREFIX}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}gpiod${CMAKE_SHARED_LIBRARY_SUFFIX}.2.1.0)
+set(LIBGPIOD_LIB_CXX ${CMAKE_INSTALL_PREFIX}/lib/${CMAKE_SHARED_LIBRARY_PREFIX}gpiodcxx${CMAKE_SHARED_LIBRARY_SUFFIX}.1.0.0)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LibGpiod FOUND_VAR LibGpiod_FOUND REQUIRED_VARS LIBGPIOD_INCLUDE_DIR LIBGPIOD_LIB_C LIBGPIOD_LIB_CXX FAIL_MESSAGE "Could not find libgpiod package")
