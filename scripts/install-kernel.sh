d0b10db8472f394bee3724d8b499b19e92f1d772
Rafael Ibasco <ribasco@gmail.com>
2018-12-07 17:53:06 +0800

    Added install-kernel script

---
+++ scripts/install-kernel.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+set -e
+
+if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-headers-4.19.0-041900_4.19.0-041900.201810221809_all.deb
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-headers-4.19.0-041900-generic_4.19.0-041900.201810221809_amd64.deb
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-image-unsigned-4.19.0-041900-generic_4.19.0-041900.201810221809_amd64.deb
+    sudo dpkg -i *.deb
+    uname -sr
+fi
