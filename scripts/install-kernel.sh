ad00518a3697f36a4dfb52adbb56ffed4abbb402
Rafael Ibasco <ribasco@gmail.com>
2018-12-07 18:27:03 +0800

    Use kernel v4.8

--- scripts/install-kernel.sh
+++ scripts/install-kernel.sh
@@ -2,9 +2,12 @@
 set -e
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-headers-4.19.0-041900_4.19.0-041900.201810221809_all.deb
-    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-headers-4.19.0-041900-generic_4.19.0-041900.201810221809_amd64.deb
-    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.19/linux-image-unsigned-4.19.0-041900-generic_4.19.0-041900.201810221809_amd64.deb
-    sudo dpkg -i *.deb
+
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.8.17/linux-headers-4.8.17-040817_4.8.17-040817.201701090438_all.deb
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.8.17/linux-headers-4.8.17-040817-generic_4.8.17-040817.201701090438_amd64.deb
+    wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.8.17/linux-image-4.8.17-040817-generic_4.8.17-040817.201701090438_amd64.deb
+    sudo dpkg -i linux-image-4.8.17-040817-generic_4.8.17-040817.201701090438_amd64.deb
+    sudo dpkg -i linux-headers-4.8.17-040817-generic_4.8.17-040817.201701090438_amd64.deb
+    sudo dpkg -i linux-headers-4.8.17-040817_4.8.17-040817.201701090438_all.deb
     uname -sr
 fi
