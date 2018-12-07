3e5d51ec7dcf6816137ff3a2847709c3984234fe
Rafael Ibasco <ribasco@gmail.com>
2018-12-07 18:34:43 +0800

    Changed order of installation

--- scripts/install-kernel.sh
+++ scripts/install-kernel.sh
@@ -2,12 +2,11 @@
 set -e
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
-
     wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.8.17/linux-headers-4.8.17-040817_4.8.17-040817.201701090438_all.deb
     wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.8.17/linux-headers-4.8.17-040817-generic_4.8.17-040817.201701090438_amd64.deb
     wget http://kernel.ubuntu.com/~kernel-ppa/mainline/v4.8.17/linux-image-4.8.17-040817-generic_4.8.17-040817.201701090438_amd64.deb
     sudo dpkg -i linux-image-4.8.17-040817-generic_4.8.17-040817.201701090438_amd64.deb
-    sudo dpkg -i linux-headers-4.8.17-040817-generic_4.8.17-040817.201701090438_amd64.deb
     sudo dpkg -i linux-headers-4.8.17-040817_4.8.17-040817.201701090438_all.deb
+    sudo dpkg -i linux-headers-4.8.17-040817-generic_4.8.17-040817.201701090438_amd64.deb
     uname -sr
 fi
