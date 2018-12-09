d0ab641b6c578ac25a732091663d25b286e7407d
Rafael Ibasco <ribasco@gmail.com>
2018-12-09 12:30:13 +0800

    Set to private

--- drivers/glcd/src/test/java/com/ibasco/ucgdisplay/drivers/glcd/GlcdConfigBuilderTest.java
+++ drivers/glcd/src/test/java/com/ibasco/ucgdisplay/drivers/glcd/GlcdConfigBuilderTest.java
@@ -33,7 +33,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class GlcdConfigBuilderTest {
 
-    GlcdConfigBuilder configBuilder;
+    private GlcdConfigBuilder configBuilder;
 
     @BeforeEach
     void setUp() {
