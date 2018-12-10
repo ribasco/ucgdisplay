42ba5390bbd4617dc6f4b10fdff464ea237dd7a4
Rafael Ibasco <ribasco@gmail.com>
2018-12-10 15:49:25 +0800

    Moved all java related operations to before_install.sh script

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -18,7 +18,17 @@ fi
 
 if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then openssl aes-256-cbc -K $encrypted_0b32ef791498_key -iv $encrypted_0b32ef791498_iv -in scripts/travis.enc -out scripts/travis.dec -d; fi
+    echo "Importing gpg secret key"
     gpg2 --batch --passphrase ${GPG_PASSPHRASE} --import scripts/travis.dec
+
+    echo "Installing Java JDK 8"
+    sudo add-apt-repository -y ppa:webupd8team/java
+    sudo apt-get update -q
+    echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
+    sudo apt-get install -y oracle-java8-installer
+    sudo apt install oracle-java8-set-default
+    export JAVA_HOME=/usr/lib/jvm/java-8-oracle
+    sudo echo "JAVA_HOME=${JAVA_HOME}"
 else
     echo "Skipping gpg key import (OS = ${TRAVIS_OS_NAME})"
 fi
