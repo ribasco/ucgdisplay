a9c45795f1411f8682c2784bb691656adaf83aaf
Rafael Ibasco <ribasco@gmail.com>
2019-09-24 05:29:09 +0800

    Removed java 8 references

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -20,13 +20,6 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then openssl aes-256-cbc -K $encrypted_0b32ef791498_key -iv $encrypted_0b32ef791498_iv -in scripts/travis.enc -out scripts/travis.dec -d; fi
     echo "Importing gpg secret key"
     gpg2 --batch --passphrase ${GPG_PASSPHRASE} --import scripts/travis.dec
-
-    #echo "Installing Java JDK 8"
-    #sudo add-apt-repository -y ppa:webupd8team/java
-    #sudo apt-get update -q
-    #echo "oracle-java8-installer shared/accepted-oracle-license-v1-1 select true" | sudo debconf-set-selections
-    #sudo apt-get install -y oracle-java8-installer
-    #sudo apt-get install oracle-java8-set-default
 else
     echo "Skipping gpg key import (OS = ${TRAVIS_OS_NAME})"
 fi
