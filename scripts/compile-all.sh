3c8783f979e4424f3485abcc1354e5de7030c39d
Rafael Ibasco <ribasco@gmail.com>
2019-11-03 23:33:35 +0800

    Updated build scripts
    Signed-off-by: Rafael Ibasco <ribasco@gmail.com>

--- scripts/compile-all.sh
+++ scripts/compile-all.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+echo "   _____ ____  __  __ _____ _____ _      ______            _      _      "
+echo "  / ____/ __ \|  \/  |  __ \_   _| |    |  ____|     /\   | |    | |     "
+echo " | |   | |  | | \  / | |__) || | | |    | |__       /  \  | |    | |     "
+echo " | |   | |  | | |\/| |  ___/ | | | |    |  __|     / /\ \ | |    | |     "
+echo " | |___| |__| | |  | | |    _| |_| |____| |____   / ____ \| |____| |____ "
+echo "  \_____\____/|_|  |_|_|   |_____|______|______| /_/    \_\______|______|"
+echo "                                                                         "
+
+
 cd /home/raffy/projects/ucgdisplay
 
 mvn clean package -P'cross-compile,!build-linux-x86_64' -DskipTests=true -Dgpg.skip
