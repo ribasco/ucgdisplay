ed5d2df5cbd43cbd6a8d2c0dba41101f3fc96922
Rafael Ibasco <ribasco@gmail.com>
2019-11-04 16:57:37 +0800

    Fixed paths
    Signed-off-by: Rafael Ibasco <ribasco@gmail.com>

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -20,6 +20,16 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then openssl aes-256-cbc -K $encrypted_0b32ef791498_key -iv $encrypted_0b32ef791498_iv -in scripts/travis.enc -out scripts/travis.dec -d; fi
     echo "Importing gpg secret key"
     gpg2 --batch --passphrase ${GPG_PASSPHRASE} --import scripts/travis.dec
+
+    echo "CC = ${CC}"
+    echo "CXX = ${CXX}"
+    clang -v
+    clang++ -v
+    which clang
+    which clang++
+    ls -l /usr/local/clang-7.0.0/bin/
+    ls -l /usr/bin
+    echo ${PATH}
 else
     echo "Skipping gpg key import (OS = ${TRAVIS_OS_NAME})"
 fi
