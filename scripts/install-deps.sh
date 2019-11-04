d2c6916a30fa2b9ac3b7575950acec754643c02f
Rafael Ibasco <ribasco@gmail.com>
2019-11-04 19:58:04 +0800

    Added tapi and cctools
    Signed-off-by: Rafael Ibasco <ribasco@gmail.com>

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -6,22 +6,38 @@ if [[ ! $TRAVIS_OS_NAME == 'linux' ]]; then
     exit 0;
 fi
 
+echo "======================================================"
 echo "Installing xar from source"
+echo "======================================================"
 
 # Install xar
-
 mkdir -p tmp
-
 cd tmp
-
 git clone https://github.com/mackyle/xar.git
-
 cd xar/xar
-
 ./autogen.sh
-
-./configure
-
 make
+sudo make install
 
+cd ../..
+
+echo "======================================================"
+echo "Installing TAPI from source"
+echo "======================================================"
+
+git clone https://github.com/tpoechtrager/apple-libtapi.git
+cd apple-libtapi
+./build.sh
+sudo ./install.sh
+
+cd ..
+
+echo "======================================================"
+echo "Installing cctools from source"
+echo "======================================================"
+
+git clone https://github.com/tpoechtrager/cctools-port.git
+cd cctools-port/cctools
+./configure
+make
 sudo make install
