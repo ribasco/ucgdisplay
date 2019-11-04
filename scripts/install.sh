310f3fefd9dcc716614c238baa7bfccabea60c4d
Rafael Ibasco <ribasco@gmail.com>
2019-11-05 01:15:13 +0800

    Accept build type argument from install script
    Signed-off-by: Rafael Ibasco <ribasco@gmail.com>

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,10 +9,31 @@ echo '  _| |_| |\  |____) |  | |/ ____ \| |____| |____ '
 echo ' |_____|_| \_|_____/   |_/_/    \_\______|______|'
 echo '                                                 '
 
-# Only cross-compile on linux (x86_64/amd64) environment
+shopt -s nocasematch
+case "$1" in
+  Debug|Release|RelWithDebInfo|MinSizeRel)
+    BUILD_TYPE=$1
+    ;;
+  *)
+    echo "Unrecognized build type argument: ${1}. Defaulting to 'Debug' type"
+    BUILD_TYPE=Debug
+    ;;
+esac
+shopt -u nocasematch
 
-if [[ ${TRAVIS_OS_NAME} == 'linux' ]]; then
-    mvn --settings scripts/settings.xml install -P'cross-compile,!build-linux-x86_64' -Dlicense.skipUpdateLicense=true -DskipTests=true -Dgpg.skip -Dmaven.javadoc.skip=true -B -V
+if [[ ${TRAVIS_OS_NAME} == 'linux' || ($(uname -o) == 'GNU/Linux' && $(uname -m) == 'x86_64') ]]; then
+    CROSS_COMPILE=yes
 else
-    mvn --settings scripts/settings.xml install -DskipTests=true -Dgpg.skip -Dmaven.javadoc.skip=true -B -V
+    CROSS_COMPILE=no
+fi
+
+MVN_PROPS="-DbuildType=${BUILD_TYPE} -Dlicense.skipUpdateLicense=true -DskipTests=true -Dgpg.skip -Dmaven.javadoc.skip=true -B -V"
+
+echo "[INSTALL] Build Type = ${BUILD_TYPE}, Cross Compile = ${CROSS_COMPILE}, Properties = ${MVN_PROPS}"
+
+# Only cross-compile on linux (x86_64/amd64) environment
+if [[ ${CROSS_COMPILE} == 'yes' ]]; then
+    mvn --settings scripts/settings.xml install -P'cross-compile,!build-linux-x86_64' ${MVN_PROPS}
+else
+    mvn --settings scripts/settings.xml install ${MVN_PROPS}
 fi
