8f662f7432a4d855541a4280b8695c1e584c6ac6
Rafael Ibasco <ribasco@gmail.com>
2019-11-04 17:59:17 +0800

    compile only mac osx 32 target
    Signed-off-by: Rafael Ibasco <ribasco@gmail.com>

--- scripts/before-install.sh
+++ scripts/before-install.sh
@@ -20,7 +20,6 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     if [[ "$TRAVIS_PULL_REQUEST" == "false" ]]; then openssl aes-256-cbc -K $encrypted_0b32ef791498_key -iv $encrypted_0b32ef791498_iv -in scripts/travis.enc -out scripts/travis.dec -d; fi
     echo "Importing gpg secret key"
     gpg2 --batch --passphrase ${GPG_PASSPHRASE} --import scripts/travis.dec
-
     echo "CC = ${CC}"
     echo "CXX = ${CXX}"
     clang -v
@@ -28,6 +27,7 @@ if [[ $TRAVIS_OS_NAME == 'linux' ]]; then
     echo "Displaying /usr/local/lib"
     ls -l /usr/local/lib
     echo ${PATH}
+    sudo find /usr -name "libLTO.so*"
 else
     echo "Skipping gpg key import (OS = ${TRAVIS_OS_NAME})"
 fi
