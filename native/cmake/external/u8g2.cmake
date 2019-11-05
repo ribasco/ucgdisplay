0276cecadd1d8cf5a4687662ffea82fa590dfa3c
Rafael Ibasco <ribasco@gmail.com>
2019-11-05 14:42:20 +0800

    Update repo
    Signed-off-by: Rafael Ibasco <ribasco@gmail.com>

--- native/cmake/external/u8g2.cmake
+++ native/cmake/external/u8g2.cmake
@@ -3,7 +3,7 @@ include(ExternalProject)
 set(PROJ_PREFIX "u8g2")
 
 #1: https://github.com/olikraus/u8g2.git
-#2: http://dev.servebeer.com/ribasco/u8g2.git
+#2: /home/raffy/projects/u8g2.git
 
 ExternalProject_Add(
         project_u8g2
