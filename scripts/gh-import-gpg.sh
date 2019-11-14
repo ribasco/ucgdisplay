829da201b60457096e13c16bb67f4e39f0b184ee
Rafael Ibasco <ribasco@gmail.com>
2019-11-14 14:01:09 +0800

    Added gpg2 package

--- scripts/gh-import-gpg.sh
+++ scripts/gh-import-gpg.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 # --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="${GPG_DECRYPT_PASSPHRASE}" --output scripts/github.asc scripts/github.enc
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_DECRYPT_PASSPHRASE" --output scripts/github.asc scripts/github.enc
 
 echo "Importing GPG key"
 
-gpg2 --batch --passphrase ${GPG_PASSPHRASE} --import scripts/github.asc
+gpg2 --batch --passphrase $GPG_PASSPHRASE --import scripts/github.asc
