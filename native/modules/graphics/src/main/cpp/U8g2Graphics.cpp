06b13d4929f18434500b013473ca4c6ffd4c5dec
Rafael Ibasco <ribasco@gmail.com>
2020-08-04 22:25:41 +0800

    Updated color

--- native/modules/graphics/src/main/cpp/U8g2Graphics.cpp
+++ native/modules/graphics/src/main/cpp/U8g2Graphics.cpp
@@ -241,10 +241,10 @@ void updateBgraBuffer(long id) {
         for (int pos = 7; pos >= 0; pos--) {
             if (data & (1 << pos)) {
                 //black
-                *(bgraBuffer++) = 255; //blue
+                *(bgraBuffer++) = 0; //blue
                 *(bgraBuffer++) = 255; //green
-                *(bgraBuffer++) = 0; //red
-                *(bgraBuffer++) = 255; //alpha
+                *(bgraBuffer++) = 255; //red
+                *(bgraBuffer++) = 0; //alpha
             } else {
                 //transparent
                 *(bgraBuffer++) = 0;
