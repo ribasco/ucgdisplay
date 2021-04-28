bd917b26fd47714d4286533017b3c662ca5fb4ca
Rafael Ibasco <13303385+ribasco@users.noreply.github.com>
2021-04-28 22:12:49 +0800

    Delete .github/scripts directory

--- .github/scripts/import-gpg.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-# --batch to prevent interactive command --yes to assume "yes" for questions
-gpg --quiet --batch --yes --decrypt --passphrase="${GPG_DECRYPT_PASSPHRASE}" --output ./.github/scripts/github.asc ./.github/scripts/github.enc
-
-echo "Importing GPG key"
-
-gpg2 --batch --passphrase ${GPG_PASSPHRASE} --import ./.github/scripts/github.asc
